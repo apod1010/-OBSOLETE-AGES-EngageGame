4cf9f098a18044742d7da700e31a2f91717fca5a
apod1010 <32077837+apod1010@users.noreply.github.com>
2018-02-26 15:24:40 -0600

    Ship health scripts, dummy ship

---
+++ Assets/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour 
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+        Animator animator;
+    //playermovement
+    //playershooting
+
+    bool isDead;
+    bool damaged;
+
+
+    private void Awake()
+    {
+
+        currentHealth = startingHealth;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        isDead = true;
+        Debug.Log("DEAD!");
+    }
+
+
+}
